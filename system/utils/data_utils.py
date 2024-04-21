07e6f9c4e5d44ddd659fecf5e33d1494923785ba
Tsing <2719584131@qq.com>
2024-04-21 17:37:49 +0800

    Update data_utils.py

--- system/utils/data_utils.py
+++ system/utils/data_utils.py
@@ -1,13 +1,13 @@
-import ujson
 import numpy as np
 import os
 import torch
+
 
 def read_data(dataset, idx, is_train=True):
     if is_train:
         train_data_dir = os.path.join('../dataset', dataset, 'train/')
 
-        train_file = train_data_dir + 'train' + str(idx) + '_.npz'
+        train_file = train_data_dir + str(idx) + '.npz'
         with open(train_file, 'rb') as f:
             train_data = np.load(f, allow_pickle=True)['data'].tolist()
 
@@ -16,7 +16,7 @@ def read_data(dataset, idx, is_train=True):
     else:
         test_data_dir = os.path.join('../dataset', dataset, 'test/')
 
-        test_file = test_data_dir + 'test' + str(idx) + '_.npz'
+        test_file = test_data_dir + str(idx) + '.npz'
         with open(test_file, 'rb') as f:
             test_data = np.load(f, allow_pickle=True)['data'].tolist()
 
@@ -24,8 +24,10 @@ def read_data(dataset, idx, is_train=True):
 
 
 def read_client_data(dataset, idx, is_train=True):
-    if dataset[:2] == "ag" or dataset[:2] == "DB":
-        return read_client_data_text(dataset, idx)
+    if "News" in dataset:
+        return read_client_data_text(dataset, idx, is_train)
+    elif "Shakespeare" in dataset:
+        return read_client_data_Shakespeare(dataset, idx)
 
     if is_train:
         train_data = read_data(dataset, idx, is_train)
@@ -65,3 +67,20 @@ def read_client_data_text(dataset, idx, is_train=True):
 
         test_data = [((x, lens), y) for x, lens, y in zip(X_test, X_test_lens, y_test)]
         return test_data
+
+
+def read_client_data_Shakespeare(dataset, idx, is_train=True):
+    if is_train:
+        train_data = read_data(dataset, idx, is_train)
+        X_train = torch.Tensor(train_data['x']).type(torch.int64)
+        y_train = torch.Tensor(train_data['y']).type(torch.int64)
+
+        train_data = [(x, y) for x, y in zip(X_train, y_train)]
+        return train_data
+    else:
+        test_data = read_data(dataset, idx, is_train)
+        X_test = torch.Tensor(test_data['x']).type(torch.int64)
+        y_test = torch.Tensor(test_data['y']).type(torch.int64)
+        test_data = [(x, y) for x, y in zip(X_test, y_test)]
+        return test_data
+
